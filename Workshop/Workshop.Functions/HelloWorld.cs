bf7f152a0bf0d14d9fb56140502a9d2de343f05b
Rayen Syal <rayensyal@outlook.com>
2023-01-09 14:31:56 +0100

    Implemented first exercise (again)

--- Workshop/Workshop.Functions/HelloWorld.cs
+++ Workshop/Workshop.Functions/HelloWorld.cs
@@ -17,19 +17,14 @@ namespace Workshop.Functions
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("Hello World has been triggered");
 
-            string name = req.Query["name"];
+            // Extract keyword and assign "World" if empty, else the keyword gets assigned
+            string keyword = req.Query["keyword"] == ((string)null) ? "World" : req.Query["keyword"];
+            string response = $"Hello {keyword}";
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            // Fire response
+            return new OkObjectResult(response);
         }
     }
 }
