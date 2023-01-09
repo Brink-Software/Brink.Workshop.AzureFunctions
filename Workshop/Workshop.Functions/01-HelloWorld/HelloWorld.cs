3d974c26fbe38bac4d8af3c89e3924a3e3a20b6e
Rayen Syal <rayensyal@outlook.com>
2023-01-09 17:01:05 +0100

    Implemented excerise 2 and 3 partially

---
+++ Workshop/Workshop.Functions/01-HelloWorld/HelloWorld.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Workshop.Functions
+{
+    public static class HelloWorld
+    {
+        [FunctionName("hello-world")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Hello World has been triggered");
+            // Extract name and assign "World" if empty, else the name gets assigned
+            string name = req.Query["name"] == ((string)null) ? "World" : req.Query["name"];
+            string response = $"Hello, {name}!";
+            // Return response
+            return new OkObjectResult(response);
+        }
+    }
+}
