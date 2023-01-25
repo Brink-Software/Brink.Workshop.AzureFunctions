53fc91275957ed96c912050730296bc6c0cbbf9a
Rayen Syal <rayensyal@outlook.com>
2023-01-25 16:06:35 +0100

    Refactored Censor to Streams

---
+++ Workshop/Workshop.Functions/03-Streams/Streams.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Workshop.Functions._03_Streams;
+
+public static class Streams
+{
+    [FunctionName("Streams")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        ILogger log)
+    {
+        log.LogInformation("Streams has been triggered");
+
+        var reader = new StreamReader(req.Body);
+        var body = reader.ReadToEnd();
+
+        string readResult = $"{body} striem";
+
+        Stream stream = new MemoryStream();
+        var writer = new StreamWriter(stream);
+        writer.Write(readResult);
+        writer.Flush();
+        stream.Position = 0;
+
+        return new OkObjectResult(stream);
+    }
+}
