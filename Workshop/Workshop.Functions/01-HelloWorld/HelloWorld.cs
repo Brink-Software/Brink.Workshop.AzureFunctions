1c170825448bec11e4020333a74b96305b7aba4e
Mark van der Salm <mark.van.der.salm@gmail.com>
2023-01-27 10:05:36 +0100

    Small clean-ups, to go with changes in the web app (#4)

--- Workshop/Workshop.Functions/01-HelloWorld/HelloWorld.cs
+++ Workshop/Workshop.Functions/01-HelloWorld/HelloWorld.cs
@@ -1,25 +1,26 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-using System.Threading.Tasks;
 
-namespace Workshop.Functions
+namespace Workshop.Functions._01_HelloWorld;
+
+public static class HelloWorld
 {
-    public static class HelloWorld
+    [FunctionName("hello-world")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        ILogger log)
     {
-        [FunctionName("hello-world")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("Hello World has been triggered");
-            // Extract name and assign "World" if empty, else the name gets assigned
-            string name = string.IsNullOrEmpty(req.Query["name"]) ? "World" : req.Query["name"];
-            string response = $"Hello, {name}!";
-            // Return response
-            return new OkObjectResult(response);
-        }
+        log.LogInformation("Hello World has been triggered");
+
+        // Extract name and assign "World" if empty, else the name gets assigned
+        string name = string.IsNullOrEmpty(req.Query["name"]) ? "world" : req.Query["name"];
+        var response = $"Hello, {name}!";
+
+        // Return response
+        return new OkObjectResult(response);
     }
 }
--- Workshop/Workshop.Functions/03-Censor/Censor.cs
+++ Workshop/Workshop.Functions/03-Censor/Censor.cs
@@ -1,35 +1,31 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System.Threading.Tasks;
 
-namespace Workshop.Functions._03_Censor
+namespace Workshop.Functions._03_Censor;
+
+public static class Censor
 {
-    public static class Censor
+    /* 
+    Het design van de applicatie wordt ontworpen door speciale UX-designers.
+    Deze designs worden vervolgens doorgespeeld naar de ontwikkelaars die deze implementeren.
+    De ontwikkelaars hebben het verzoek van de UX-designers gekregen om de creditcardgegevens van klanten beter te censureren.
+    Dit is tevens ook een beveiligingsrisico.
+
+    Kan jij het verzoek verwerken?
+     */
+    [FunctionName("Censor")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        ILogger log)
     {
-        /* 
-        Het design van de applicatie wordt ontworpen door speciale UX-designers.
-        Deze designs worden vervolgens doorgespeeld naar de ontwikkelaars die deze implementeren.
-        De ontwikkelaars hebben het verzoek van de UX-designers gekregen om de creditcardgegevens van klanten beter te censureren.
-        Dit is tevens ook een beveiligingsrisico.
+        log.LogInformation("Censor has been triggered");
+        string keyword = req.Query["keyword"];
 
-        Kan jij het verzoek verwerken?
-         */
-        [FunctionName("Censor")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("Censor has been triggered");
-            string keyword = req.Query["keyword"];
-
-            string response = "";
-            return new OkObjectResult(response);
-        }
+        string response = "";
+        return new OkObjectResult(response);
     }
 }
